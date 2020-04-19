3013f776b7a7616071abd390608b3cffe0f967c7
UofMTigerFan <wsl15a@acu.edu>
2020-04-18 20:20:00 -0500

    Add files via upload

---
+++ GoogleHappy-master/src/main/java/GoogleHappy.java
@@ -0,0 +1,356 @@
+import java.util.*;
+import java.io.*;
+
+/******************************************************************************
+ *  Author : 
+ *  Class  : Spring 2020 Dr. Reeves CS374
+ *  Task   : Do something for the project
+ *  Date   : 5/6/2020
+ *
+ *
+ *
+ ******************************************************************************/
+public class GoogleHappy 
+{
+
+  private String weighted;
+
+  public double getMath(double n, int choice)
+  {
+    if (weighted == "t")
+    {
+      int count = 0;
+
+      for (int i = 0; i <= n; i++)
+      {
+        count += i;
+      }
+      return (n-choice+1)/count;
+    }
+
+    return 1/n;
+    
+  }
+
+
+  public class PageRank {
+   
+      public int path[][] = new int[10000][10000];
+      public double pagerank[] = new double[10000];
+   
+      public void calc(double totalNodes){
+      
+    double InitialPageRank;
+    double OutgoingLinks=0; 
+    double DampingFactor = 0.85; 
+    double TempPageRank[] = new double[10000];
+
+    int ExternalNodeNumber;
+    int InternalNodeNumber; 
+    int k=1; // For Traversing
+    int ITERATION_STEP=1;
+
+    InitialPageRank = 1/totalNodes;
+    System.out.printf(" Total Number of Nodes :"+totalNodes+"\t Initial PageRank  of All Nodes :"+InitialPageRank+"\n");
+   
+    // 0th ITERATION  _ OR _ INITIALIZATION PHASE
+    for(k=1;k<=totalNodes;k++)
+        {
+      this.pagerank[k]=InitialPageRank;
+        }   
+    
+    System.out.printf("\n Initial PageRank Values , 0th Step \n");
+    for(k=1;k<=totalNodes;k++)
+        {
+      System.out.printf(" Page Rank of "+k+" is :\t"+this.pagerank[k]+"\n");
+        }  
+    
+    while(ITERATION_STEP<=2) // Iterations
+        {
+      // Store the PageRank for All Nodes in Temporary Array 
+      for(k=1;k<=totalNodes;k++)
+          {  
+        TempPageRank[k]=this.pagerank[k];
+        this.pagerank[k]=0;
+          }
+      
+      for(InternalNodeNumber=1;InternalNodeNumber<=totalNodes;InternalNodeNumber++)
+          {
+        for(ExternalNodeNumber=1;ExternalNodeNumber<=totalNodes;ExternalNodeNumber++)
+            {
+          if(this.path[ExternalNodeNumber][InternalNodeNumber] > 0)
+              { 
+            k=1;
+            OutgoingLinks=0;  // Count the Number of Outgoing Links for each ExternalNodeNumber
+            while(k<=totalNodes)
+                {
+              if(this.path[ExternalNodeNumber][k] > 0 )
+                  {
+                OutgoingLinks=OutgoingLinks+1; // Counter for Outgoing Links
+                  }  
+              k=k+1;  
+                } 
+            // Calculate PageRank     
+            this.pagerank[InternalNodeNumber]+=TempPageRank[ExternalNodeNumber]*(getMath(OutgoingLinks,path[ExternalNodeNumber][InternalNodeNumber]));    
+              }
+            }  
+          }    
+       
+      System.out.printf("\n After "+ITERATION_STEP+"th Step \n");
+    
+      for(k=1;k<=totalNodes;k++) 
+          System.out.printf(" Page Rank of "+k+" is :\t"+this.pagerank[k]+"\n"); 
+    
+      ITERATION_STEP = ITERATION_STEP+1;
+        }
+
+    // Add the Damping Factor to PageRank
+    for(k=1;k<=totalNodes;k++)
+        {
+      this.pagerank[k]=(1-DampingFactor)+ DampingFactor*this.pagerank[k]; 
+        } 
+    
+    // Display PageRank
+    System.out.printf("\n Final Page Rank : \n"); 
+    for(k=1;k<=totalNodes;k++)
+        {
+      System.out.printf(" Page Rank of "+k+" is :\t"+this.pagerank[k]+"\n"); 
+        }
+    
+      } 
+
+
+  }
+
+
+
+
+
+
+
+
+  public class User
+  {
+    public int total_prefs;
+    public int id;
+    public String name;
+
+    User(String[] p, int i)
+    {
+      name = p[0];
+      total_prefs = p.length - 1;
+      id = i;
+    }
+    User(String p, int i)
+    {
+      name = p;
+      id = i;
+      total_prefs = 0;
+    }
+
+  }
+
+
+  public class Team
+  {
+    public int max;
+    public int current;
+    public User people[];
+
+    Team(int n, User[] people)
+    {
+      max = n;
+      int count = 0;
+      for (User temp : people)
+      {
+        people[count] = temp;
+        count ++;
+      }
+      current = count;
+    }
+  }
+
+  
+
+
+
+
+  //primary list of people
+  public int count;
+  public int teamsize;
+  public PageRank p;
+  public User[] c;
+
+  //reads from file and puts users into the people array
+  private void prefs(HashMap<String, Integer> mentioned_people)
+  {
+    count = 0;
+    c = new User[10000];
+    Scanner scanner = new Scanner(System.in);
+
+    String[] temp = new String[10000];
+
+    //reads lines into temp array
+    while(scanner.hasNextLine())
+    {
+      temp[count] = scanner.nextLine();
+      //processes line and add name to set
+      String[] temp2 = temp[count].split(",");
+
+      String[] temp3 = Arrays.copyOfRange(temp2, 1, temp2.length);
+
+      if(!mentioned_people.containsKey(temp2[0]))
+      {
+        c[count] = new User(temp2, count + 1);
+        mentioned_people.put(temp2[0],count); 
+
+      }
+      count ++;
+
+    }
+
+    int temp_n = count;
+    //creates users now that the total number of them is known
+    for (int i = 0; i < count; i++)
+    {
+
+      String[] temp2 = temp[i].split(",");
+
+      String[] temp3 = Arrays.copyOfRange(temp2, 1, temp2.length);
+      int len = temp3.length;
+
+      for (int j = 0; j < len; j++)
+      {
+        //goes through choices and if it is not in the map adds it
+        if (!mentioned_people.containsKey(temp3[j]))
+        {
+          temp[count] = temp3[j];
+          c[count] = new User(temp3[j], count + 1);
+          mentioned_people.put(temp3[j],count); 
+          count ++;
+
+        }
+
+
+      }
+
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+      String[] temp2 = temp[i].split(",");
+      String[] temp3 = Arrays.copyOfRange(temp2, 1, temp2.length);
+
+      //sets all paths to 0
+      for (int j = 0; j < count; j++)
+      {
+        p.path[i+1][j+1] = 0;
+      }
+
+      //chages paths based on existing prefs
+      for (int j = 0; j < temp3.length; j++)
+      {
+        int t = mentioned_people.get(temp3[j]);
+        p.path[i+1][t+1] = 1;
+        if (weighted == "t")
+        {
+          p.path[i+1][t+1] = j+1;
+        }
+
+      }
+    }
+
+    for (int i = 1; i <= count; i++)
+    {
+      for (int j = 1; j <= count; j++)
+      {
+                //sets to zero if point at self
+        if(j==i)
+        {
+          p.path[i][j]=0;
+        }
+      }
+
+    }
+    System.out.println("\n\n");
+
+    for (int i = 1; i <= count; i++)
+    {
+      String line = "";
+      for (int j = 1 ; j <= count; j++)
+      {
+        line = line + p.path[i][j];
+      }
+      System.out.println(line);
+    }
+
+    scanner.close();
+  }
+  
+  public void primaryFunction(String w)
+  {
+    weighted = w;
+    teamsize = 0;
+    //defines map
+    HashMap<String, Integer> mentioned_people = new HashMap<String, Integer>();
+    p = new PageRank();
+
+    //loads the users and their prefs
+    prefs(mentioned_people);
+
+    //returns an array of all the names and outputs them into teams of 3 
+    getKeys(mentioned_people, 0);
+    
+    //runs page rank
+    p.calc(count);
+
+    //potential loop for multiple itterations???
+
+    //run team optimization algorithm
+
+    //output (with verbosity??)
+  }
+
+  public static void main (String[] args )  throws FileNotFoundException
+  {
+    //creates the main GoogleHappy object and runs primaryFunction
+    new GoogleHappy().primaryFunction(args[0]);
+  }
+  
+  //send in the hashmap,0 to just set an array send in hashmap,1 to output a set of teams
+  public static String[] getKeys(HashMap<String, Integer> hash, int onOff)
+  {
+	  
+	//Makes an array of just the names of people
+	String allNames[] = new String [hash.size()];
+	int i = 0;
+	for (String key : hash.keySet()) 
+	{
+		allNames[i] = key;
+		i++;
+	}
+
+	if(onOff == 1)
+	{
+		//print out the names and divide them into groups of 3
+		System.out.println("\nTEAMS\n-------");
+		i = 1;
+		for( int n = 0; n < allNames.length; n++)
+		{
+			if(n%3==0)
+			{
+				if(n!=0&&n!=1)
+					System.out.print("\n");
+				System.out.println("Team " + (i));
+				i++;
+			}
+			
+			System.out.println(allNames[n]);
+		} 
+	}
+	 
+	//returns an array of names
+	return allNames;
+  }
+  
+}
---
+++ GoogleHappy-master/src/test/java/GoogleHappyTest.java
@@ -0,0 +1,610 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import org.junit.*;
+import java.util.*;
+import java.io.*;
+
+
+
+
+/******************************************************************************
+ *  Author : bal15b Ben Lamont
+ *  Class  : Spring 2020 Dr. Reeves CS374
+ *  Task   : Do Somehting test
+ ******************************************************************************/
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class GoogleHappyTest
+{
+    @Test
+    public void atest1()  throws FileNotFoundException
+    {
+    	//Test 1 By Ben Lamont
+		System.out.println("\n------TEST 1------");
+    	GoogleHappy g = new GoogleHappy();
+
+    	String prefs = "A,B\nB,E\nC,A,B,D,E\nD,C,E\nE,D";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("f");
+
+        int t = (int)(g.p.pagerank[1] * 100);
+        assertEquals(t,17);
+
+        t = (int)(g.p.pagerank[2] * 100);
+        assertEquals(t,21);
+
+        t = (int)(g.p.pagerank[3] * 100);
+        assertEquals(t,25);
+
+        t = (int)(g.p.pagerank[4] * 100);
+        assertEquals(t,46);
+
+        t = (int)(g.p.pagerank[5] * 100);
+        assertEquals(t,49);
+    }
+
+    @Test
+    public void btest2()  throws FileNotFoundException
+    {
+    	//Test 2 By Ben Lamont
+		System.out.println("\n------TEST 2------");
+    	GoogleHappy g = new GoogleHappy();
+
+    	String prefs = "Calvin,Bubba,Blinkendorfer,Suzie\nBubba,Snoopie,Donald\nBlinkendorfer,Bubba,Billy,Ivanka\nSuzie,Ivanka,Donald\nDonald,Bubba,Ivanka,Mitch\nMitch,Snoopie,Bubba,Suzie,Ivanka,Donald\nSnoopie,Bubba\nBilly,Donald,Blinkendorfer\nIvanka,Donald,Billy,Suzie";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("f");
+
+		int t = (int)(g.p.pagerank[1] * 100);
+        assertEquals(t,15);
+
+        t = (int)(g.p.pagerank[2] * 100);
+        assertEquals(t,31);
+
+        t = (int)(g.p.pagerank[3] * 100);
+        assertEquals(t,18);
+
+        t = (int)(g.p.pagerank[4] * 100);
+        assertEquals(t,19);
+
+        t = (int)(g.p.pagerank[5] * 100);
+        assertEquals(t,37);
+
+        t = (int)(g.p.pagerank[6] * 100);
+        assertEquals(t,21);
+
+        t = (int)(g.p.pagerank[7] * 100);
+        assertEquals(t,26);
+
+        t = (int)(g.p.pagerank[8] * 100);
+        assertEquals(t,21);
+
+        t = (int)(g.p.pagerank[9] * 100);
+        assertEquals(t,28);
+    }
+
+    //test 3 makes sure the matrix is alligned correctly
+    @Test
+    public void ctest3()  throws FileNotFoundException
+    {
+    	//Test 3 By Nic Fornicola
+		System.out.println("\n------TEST 3------");
+    	GoogleHappy g = new GoogleHappy();
+
+    	String prefs = "Calvin,Bubba,Blinkendorfer,Suzie\nBubba,Snoopie,Donald\nBlinkendorfer,Bubba,Billy,Ivanka\nSuzie,Ivanka,Donald\nDonald,Bubba,Ivanka,Mitch\nMitch,Snoopie,Bubba,Suzie,Ivanka,Donald\nSnoopie,Bubba\nBilly,Donald,Blinkendorfer\nIvanka,Donald,Billy,Suzie";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+		
+        g.primaryFunction("f");
+		
+		assertEquals(g.p.path[1][1],0);
+		assertEquals(g.p.path[1][2],1);
+		assertEquals(g.p.path[1][3],1);
+		assertEquals(g.p.path[1][4],1);
+		assertEquals(g.p.path[1][5],0);
+		assertEquals(g.p.path[1][6],0);
+		assertEquals(g.p.path[1][7],0);
+		assertEquals(g.p.path[1][8],0);
+		assertEquals(g.p.path[1][9],0);
+
+		assertEquals(g.p.path[2][1],0);
+		assertEquals(g.p.path[2][2],0);
+		assertEquals(g.p.path[2][3],0);
+		assertEquals(g.p.path[2][4],0);
+		assertEquals(g.p.path[2][5],1);
+		assertEquals(g.p.path[2][6],0);
+		assertEquals(g.p.path[2][7],1);
+		assertEquals(g.p.path[2][8],0);
+		assertEquals(g.p.path[2][9],0);
+
+		assertEquals(g.p.path[3][1],0);
+		assertEquals(g.p.path[3][2],1);
+		assertEquals(g.p.path[3][3],0);
+		assertEquals(g.p.path[3][4],0);
+		assertEquals(g.p.path[3][5],0);
+		assertEquals(g.p.path[3][6],0);
+		assertEquals(g.p.path[3][7],0);
+		assertEquals(g.p.path[3][8],1);
+		assertEquals(g.p.path[3][9],1);
+
+    }
+	
+    //test 4 makes sure the matrix is alligned correctly
+    @Test
+    public void dtest4()  throws FileNotFoundException
+    {
+    	//Test 4 By Nic Fornicola
+		System.out.println("\n------TEST 4------");
+    	GoogleHappy g = new GoogleHappy();
+
+	//different preferences
+    	String prefs = "Calvin,Bubba,Suzie,Billy,Ivanka,Snoopie,Donald\nBubba,Snoopie,Suzie,Donald,Billy,\nBlinkendorfer,Billy,Ivanka\nSuzie,Donald,Blinkendorfer,Bubba\nDonald,Bubba,Ivanka,Mitch\nMitch,Bubba,Suzie,Ivanka,Donald\nSnoopie,Bubba\nBilly,Donald,Blinkendorfer,Snoopie\nIvanka,Donald,Suzie";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+		
+        g.primaryFunction("f");
+		
+		assertEquals(g.p.path[1][1],0);
+		assertEquals(g.p.path[1][2],1);
+		assertEquals(g.p.path[1][3],0);
+		assertEquals(g.p.path[1][4],1);
+		assertEquals(g.p.path[1][5],1);
+		assertEquals(g.p.path[1][6],0);
+		assertEquals(g.p.path[1][7],1);
+		assertEquals(g.p.path[1][8],1);
+		assertEquals(g.p.path[1][9],1);
+
+		assertEquals(g.p.path[2][1],0);
+		assertEquals(g.p.path[2][2],0);
+		assertEquals(g.p.path[2][3],0);
+		assertEquals(g.p.path[2][4],1);
+		assertEquals(g.p.path[2][5],1);
+		assertEquals(g.p.path[2][6],0);
+		assertEquals(g.p.path[2][7],1);
+		assertEquals(g.p.path[2][8],1);
+		assertEquals(g.p.path[2][9],0);
+
+		assertEquals(g.p.path[3][1],0);
+		assertEquals(g.p.path[3][2],0);
+		assertEquals(g.p.path[3][3],0);
+		assertEquals(g.p.path[3][4],0);
+		assertEquals(g.p.path[3][5],0);
+		assertEquals(g.p.path[3][6],0);
+		assertEquals(g.p.path[3][7],0);
+		assertEquals(g.p.path[3][8],1);
+		assertEquals(g.p.path[3][9],1);
+		
+    }
+
+    //makes sure matrix includes people who are only mentioned
+    @Test
+    public void etest5()  throws FileNotFoundException
+    {
+    	//Test 5 By Ben Lamont
+		System.out.println("\n------TEST 5------");
+    	GoogleHappy g = new GoogleHappy();
+
+    	String prefs = "A,B\nB,C,D,E\nE,D,A";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("f");
+
+       	assertEquals(g.p.path[1][1],0);
+       	assertEquals(g.p.path[2][2],0);
+       	assertEquals(g.p.path[3][3],0);
+       	assertEquals(g.p.path[4][4],0);
+       	assertEquals(g.p.path[5][5],0);
+
+       	assertEquals(g.p.path[1][2],1);
+       	assertEquals(g.p.path[2][3],1);
+       	assertEquals(g.p.path[2][4],1);
+       	assertEquals(g.p.path[2][5],1);
+       	assertEquals(g.p.path[3][1],1);
+       	assertEquals(g.p.path[3][5],1);
+    }
+
+
+    //test to see if it breaks with no people
+    @Test
+    public void ftest6()  throws FileNotFoundException
+    {
+    	//Test 6 By Ben Lamont
+		System.out.println("\n------TEST 6------");
+    	GoogleHappy g = new GoogleHappy();
+
+    	String prefs = "";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("f");
+
+        assertEquals(g.count,0);
+
+    }
+
+    //makes sure the weighted values are entered into the array correctly
+    @Test
+    public void gtest7()  throws FileNotFoundException
+    {
+    	//Test 7 By Ben Lamont
+		System.out.println("\n------TEST 7------");
+    	GoogleHappy g = new GoogleHappy();
+
+    	String prefs = "A,B,C\nB,C\nC,B,A,D";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("t");
+
+        assertEquals(g.p.path[1][1],0);
+        assertEquals(g.p.path[2][2],0);
+        assertEquals(g.p.path[3][3],0);
+        assertEquals(g.p.path[4][4],0);
+
+        assertEquals(g.p.path[1][2],1);
+        assertEquals(g.p.path[1][3],2);
+
+        assertEquals(g.p.path[2][3],1);
+
+        assertEquals(g.p.path[3][2],1);
+        assertEquals(g.p.path[3][1],2);
+        assertEquals(g.p.path[3][4],3);
+
+    }
+
+
+    //tests to make sure you can't vote for yourself
+    @Test
+    public void htest8()  throws FileNotFoundException
+    {
+    	//Test 8 By Ben Lamont
+		System.out.println("\n------TEST 8------");
+    	GoogleHappy g = new GoogleHappy();
+
+    	String prefs = "A,A,A,A\nB,B,B,B\nC,C,C,C\nD,D,D,D";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("f");
+
+        for (int i = 1; i <= 4; i++)
+        {
+        	for (int j = 1; j <= 4; j++)
+        	{
+        		assertEquals(g.p.path[i][j],0);
+        	}
+        }
+
+
+    }
+
+
+    //tests to make sure it doesn't break on an empty preference
+    @Test
+    public void itest9()  throws FileNotFoundException
+    {
+    	//Test 9 By Ben Lamont
+		System.out.println("\n------TEST 9------");
+    	GoogleHappy g = new GoogleHappy();
+
+    	String prefs = "A,B,C\nB,C\nC,B,,D";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("f");
+
+        assertEquals(g.p.path[3][4],1);
+    }
+
+
+    //tests to make sure weighted prefs are distributed properly
+    @Test
+    public void jtest10()  throws FileNotFoundException
+    {
+    	//Test 10 By Ben Lamont
+		System.out.println("\n------TEST 10------");
+    	GoogleHappy g = new GoogleHappy();
+
+    	String prefs = "1,2,3,4,5,6,7,8,9\n2,1,3,4,5,6,7,8,9\n3,1,2,4,5,6,7,8,9\n4,1,2,3,5,6,7,8,9\n5,1,2,3,4,6,7,8,9\n6,1,2,3,4,5,7,8,9\n7,1,2,3,4,5,6,8,9\n8,1,2,3,4,5,6,7,9\n9,1,2,3,4,5,6,7,8";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("t");
+
+
+        for (int i = 1; i < 9; i++)
+        {
+        	assertTrue(g.p.pagerank[i] > g.p.pagerank[i+1]);
+        }
+
+    }
+	
+	@Test
+    public void ktest11()  throws FileNotFoundException
+    {
+		
+	//Test 11 By Nic Fornicola
+	System.out.println("\n------TEST 11------");
+    	GoogleHappy g = new GoogleHappy();
+		
+		
+		HashMap<String, Integer> testHash = new HashMap<String, Integer>();
+		testHash.put("AA",0);
+		testHash.put("BB",1);
+		testHash.put("CC",2);
+		testHash.put("DD",3);
+		testHash.put("EE",4);
+		String allNames[] = new String [5];
+		
+		//See keys and values
+		//System.out.println(testHash.entrySet());
+		
+		allNames = g.getKeys(testHash, 1);
+		
+		assertEquals(allNames[0],"AA");
+		assertEquals(allNames[1],"BB");
+		assertEquals(allNames[2],"CC");
+		assertEquals(allNames[3],"DD");
+		assertEquals(allNames[4],"EE");
+		
+
+    }
+	
+	@Test
+    public void ltest12()  throws FileNotFoundException
+    {
+		
+	//Test 12 By Nic Fornicola
+	System.out.println("\n------TEST 12------");
+    	GoogleHappy g = new GoogleHappy();
+		
+		
+		HashMap<String, Integer> testHash = new HashMap<String, Integer>();
+		testHash.put("C",0);
+		testHash.put("E",1);
+		testHash.put("A",2);
+		testHash.put("D",3);
+		testHash.put("B",4);
+		String allNames[] = new String[5];
+		
+		//See keys and values
+		//System.out.println(testHash.entrySet());
+		
+		allNames = g.getKeys(testHash, 0);
+		
+		assertEquals(allNames[0],"A");
+		assertEquals(allNames[1],"B");
+		assertEquals(allNames[2],"C");
+		assertEquals(allNames[3],"D");
+		assertEquals(allNames[4],"E");
+
+
+    }
+	
+	@Test
+    public void mtest13()  throws FileNotFoundException
+    {
+		
+	//Test 13 By Nic Fornicola
+	System.out.println("\n------TEST 13------");
+    	GoogleHappy g = new GoogleHappy();
+		
+		
+		HashMap<String, Integer> testHash = new HashMap<String, Integer>();
+		testHash.put("1",0);
+		testHash.put("2",0);
+		testHash.put("3",0);
+		testHash.put("4",0);
+		testHash.put("5",0);
+		String allNames[] = new String[5];
+		
+		//See keys and values
+		//System.out.println(testHash.entrySet());
+		
+		allNames = g.getKeys(testHash, 0);
+		
+		assertEquals(allNames[0],"1");
+		assertEquals(allNames[1],"2");
+		assertEquals(allNames[2],"3");
+		assertEquals(allNames[3],"4");
+		assertEquals(allNames[4],"5");
+	
+
+    }
+	
+	@Test
+    public void ntest14()  throws FileNotFoundException
+    {
+		
+	//Test 14 By Nic Fornicola
+	System.out.println("\n------TEST 14------");
+    	GoogleHappy g = new GoogleHappy();
+		
+		
+		HashMap<String, Integer> testHash = new HashMap<String, Integer>();
+		testHash.put("11",0);
+		testHash.put("22",0);
+		testHash.put("33",0);
+		testHash.put("44",0);
+		testHash.put("55",0);
+		testHash.put("66",0);
+		testHash.put("77",0);
+		testHash.put("88",0);
+		testHash.put("99",0);
+		testHash.put("100",0);
+		String allNames[] = new String[5];
+		
+		//See keys and values
+		//System.out.println(testHash.entrySet());
+		
+		allNames = g.getKeys(testHash, 0);
+		
+		assertEquals(allNames[0],"11");
+		assertEquals(allNames[1],"22");
+		assertEquals(allNames[2],"33");
+		assertEquals(allNames[3],"44");
+		assertEquals(allNames[4],"55");
+		assertEquals(allNames[5],"66");
+		assertEquals(allNames[6],"77");
+		assertEquals(allNames[7],"88");
+		assertEquals(allNames[8],"99");
+		assertEquals(allNames[9],"100");	
+
+    }
+	
+		@Test
+    public void otest15()  throws FileNotFoundException
+    {
+		
+	//Test 15 By Nic Fornicola
+	System.out.println("\n------TEST 15------");
+    	GoogleHappy g = new GoogleHappy();
+		
+		
+		HashMap<String, Integer> testHash = new HashMap<String, Integer>();
+		testHash.put("a",0);
+		testHash.put("e",1);
+		testHash.put("h",2);
+		testHash.put("k",3);
+		testHash.put("n",4);
+
+		String allNames[] = new String[5];
+		
+		//See keys and values
+		//System.out.println(testHash.entrySet());
+		
+		allNames = g.getKeys(testHash, 0);
+		
+		assertEquals(allNames[0],"a");
+		assertEquals(allNames[1],"e");
+		assertEquals(allNames[2],"h");
+		assertEquals(allNames[3],"k");
+		assertEquals(allNames[4],"n");	
+
+    }
+
+	@Test
+    public void ptest16()  throws FileNotFoundException
+    {
+		//test 16 by Ben Lamont
+		System.out.println("\n------TEST 16------");
+    	GoogleHappy g = new GoogleHappy();
+
+		String prefs = "A,A,A,A\nB,B,B,B\nC,C,C,C\nD,D,D,D";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+		g.primaryFunction("f");
+
+		assertEquals(g.c[0].name,"A");
+		assertEquals(g.c[1].name,"B");
+		assertEquals(g.c[2].name,"C");
+		assertEquals(g.c[3].name,"D");
+
+	}
+
+	@Test
+    public void qtest17()  throws FileNotFoundException
+    {
+		//test 16 by Ben Lamont
+		System.out.println("\n------TEST 17------");
+    	GoogleHappy g = new GoogleHappy();
+
+		String prefs = "A,A,A,A\nB,B,B,B\nC,C,C,D\nE,E,E,E";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("f");
+
+		assertEquals(g.c[0].name,"A");
+		assertEquals(g.c[1].name,"B");
+		assertEquals(g.c[2].name,"C");
+		assertEquals(g.c[3].name,"E");
+		assertEquals(g.c[4].name,"D");
+	}
+
+	@Test
+    public void rtest18()  throws FileNotFoundException
+    {
+		//test 16 by Ben Lamont
+		System.out.println("\n------TEST 18------");
+    	GoogleHappy g = new GoogleHappy();
+
+		String prefs = "A,A,A,A\nB,B,B,B\nC,C,,E\nD,D,D,D";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("f");
+
+		assertEquals(g.c[0].name,"A");
+		assertEquals(g.c[1].name,"B");
+		assertEquals(g.c[2].name,"C");
+		assertEquals(g.c[3].name,"D");
+		assertEquals(g.c[4].name,"");
+		assertEquals(g.c[5].name,"E");
+
+	}
+
+	@Test
+    public void stest19()  throws FileNotFoundException
+    {
+		//test 16 by Ben Lamont
+		System.out.println("\n------TEST 19------");
+    	GoogleHappy g = new GoogleHappy();
+
+		String prefs = "B,A";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("f");
+
+		assertEquals(g.c[0].name,"B");
+		assertEquals(g.c[1].name,"A");
+	}
+
+	@Test
+    public void ttest20()  throws FileNotFoundException
+    {
+		//test 20 by Ben Lamont
+		System.out.println("\n------TEST 20------");
+    	GoogleHappy g = new GoogleHappy();
+
+		String prefs = "A,BC\nB,C\nC,A";
+
+    	ByteArrayInputStream in = new ByteArrayInputStream(prefs.getBytes());
+        System.setIn(in);
+
+        g.primaryFunction("f");
+
+		assertEquals(g.c[0].name,"A");
+		assertEquals(g.c[1].name,"B");
+		assertEquals(g.c[2].name,"C");
+		assertEquals(g.c[3].name,"BC");
+
+	}
+}
