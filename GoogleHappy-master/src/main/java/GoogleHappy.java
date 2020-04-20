795353f8e9dab4ae436b2d4bf4d1945bd2ace577
ndf17a <57545704+ndf17a@users.noreply.github.com>
2020-04-19 19:19:15 -0500

    fixed bug

--- GoogleHappy-master/src/main/java/GoogleHappy.java
+++ GoogleHappy-master/src/main/java/GoogleHappy.java
@@ -1,3 +1,4 @@
+import java.util.Arrays; 
 import java.util.*;
 import java.io.*;
 
@@ -13,123 +14,118 @@ import java.io.*;
 public class GoogleHappy 
 {
 
-  private String weighted;
+	private String weighted;
 
-  public double getMath(double n, int choice)
-  {
-    if (weighted == "t")
-    {
-      int count = 0;
+	public double getMath(double n, int choice)
+	{
+		if (weighted == "t")
+		{
+			int count = 0;
 
-      for (int i = 0; i <= n; i++)
-      {
-        count += i;
-      }
-      return (n-choice+1)/count;
-    }
+			for (int i = 0; i <= n; i++)
+				count += i;
+			
+			return (n-choice+1)/count;
+		}
 
-    return 1/n;
-    
-  }
-
-
-  public class PageRank {
-   
-      public int path[][] = new int[10000][10000];
-      public double pagerank[] = new double[10000];
-   
-      public void calc(double totalNodes){
-      
-    double InitialPageRank;
-    double OutgoingLinks=0; 
-    double DampingFactor = 0.85; 
-    double TempPageRank[] = new double[10000];
-
-    int ExternalNodeNumber;
-    int InternalNodeNumber; 
-    int k=1; // For Traversing
-    int ITERATION_STEP=1;
-
-    InitialPageRank = 1/totalNodes;
-    System.out.printf(" Total Number of Nodes :"+totalNodes+"\t Initial PageRank  of All Nodes :"+InitialPageRank+"\n");
-   
-    // 0th ITERATION  _ OR _ INITIALIZATION PHASE
-    for(k=1;k<=totalNodes;k++)
-        {
-      this.pagerank[k]=InitialPageRank;
-        }   
-    
-    System.out.printf("\n Initial PageRank Values , 0th Step \n");
-    for(k=1;k<=totalNodes;k++)
-        {
-      System.out.printf(" Page Rank of "+k+" is :\t"+this.pagerank[k]+"\n");
-        }  
-    
-    while(ITERATION_STEP<=2) // Iterations
-        {
-      // Store the PageRank for All Nodes in Temporary Array 
-      for(k=1;k<=totalNodes;k++)
-          {  
-        TempPageRank[k]=this.pagerank[k];
-        this.pagerank[k]=0;
-          }
-      
-      for(InternalNodeNumber=1;InternalNodeNumber<=totalNodes;InternalNodeNumber++)
-          {
-        for(ExternalNodeNumber=1;ExternalNodeNumber<=totalNodes;ExternalNodeNumber++)
-            {
-          if(this.path[ExternalNodeNumber][InternalNodeNumber] > 0)
-              { 
-            k=1;
-            OutgoingLinks=0;  // Count the Number of Outgoing Links for each ExternalNodeNumber
-            while(k<=totalNodes)
-                {
-              if(this.path[ExternalNodeNumber][k] > 0 )
-                  {
-                OutgoingLinks=OutgoingLinks+1; // Counter for Outgoing Links
-                  }  
-              k=k+1;  
-                } 
-            // Calculate PageRank     
-            this.pagerank[InternalNodeNumber]+=TempPageRank[ExternalNodeNumber]*(getMath(OutgoingLinks,path[ExternalNodeNumber][InternalNodeNumber]));    
-              }
-            }  
-          }    
-       
-      System.out.printf("\n After "+ITERATION_STEP+"th Step \n");
-    
-      for(k=1;k<=totalNodes;k++) 
-          System.out.printf(" Page Rank of "+k+" is :\t"+this.pagerank[k]+"\n"); 
-    
-      ITERATION_STEP = ITERATION_STEP+1;
-        }
-
-    // Add the Damping Factor to PageRank
-    for(k=1;k<=totalNodes;k++)
-        {
-      this.pagerank[k]=(1-DampingFactor)+ DampingFactor*this.pagerank[k]; 
-        } 
-    
-    // Display PageRank
-    System.out.printf("\n Final Page Rank : \n"); 
-    for(k=1;k<=totalNodes;k++)
-        {
-      System.out.printf(" Page Rank of "+k+" is :\t"+this.pagerank[k]+"\n"); 
-        }
-    
-      } 
+		return 1/n;
+		
+	}
 
 
-  }
+	public class PageRank 
+	{
+	   
+		public int path[][] = new int[10000][10000];
+		public double pagerank[] = new double[10000];
+	   
+		public void calc(double totalNodes)
+		{
+		  
+			double InitialPageRank;
+			double OutgoingLinks=0; 
+			double DampingFactor = 0.85; 
+			double TempPageRank[] = new double[10000];
+
+			int ExternalNodeNumber;
+			int InternalNodeNumber; 
+			int k=1; // For Traversing
+			int ITERATION_STEP=1;
+
+			InitialPageRank = 1/totalNodes;
+			System.out.printf(" Total Number of Nodes :"+totalNodes+"\t Initial PageRank  of All Nodes :"+InitialPageRank+"\n");
+		   
+			// 0th ITERATION  _ OR _ INITIALIZATION PHASE
+			for(k=1;k<=totalNodes;k++)
+			{
+			  this.pagerank[k]=InitialPageRank;
+			}   
+			
+			System.out.printf("\n Initial PageRank Values , 0th Step \n");
+			for(k=1;k<=totalNodes;k++)
+			{
+				System.out.printf(" Page Rank of "+k+" is :\t"+this.pagerank[k]+"\n");
+			}  
+			
+			while(ITERATION_STEP<=2) // Iterations
+			{
+				// Store the PageRank for All Nodes in Temporary Array 
+				for(k=1;k<=totalNodes;k++)
+					{  
+						TempPageRank[k]=this.pagerank[k];
+						this.pagerank[k]=0;
+					}
+				  
+				for(InternalNodeNumber=1;InternalNodeNumber<=totalNodes;InternalNodeNumber++)
+				{
+					for(ExternalNodeNumber=1;ExternalNodeNumber<=totalNodes;ExternalNodeNumber++)
+					{
+						if(this.path[ExternalNodeNumber][InternalNodeNumber] > 0)
+						{ 
+							k=1;
+							OutgoingLinks=0;  // Count the Number of Outgoing Links for each ExternalNodeNumber
+							while(k<=totalNodes)
+							{
+								if(this.path[ExternalNodeNumber][k] > 0 )
+								{
+									OutgoingLinks=OutgoingLinks+1; // Counter for Outgoing Links
+								}  
+								k=k+1;  
+							}
+							
+							// Calculate PageRank     
+							this.pagerank[InternalNodeNumber]+=TempPageRank[ExternalNodeNumber]*(getMath(OutgoingLinks,path[ExternalNodeNumber][InternalNodeNumber]));    
+						}
+					}  
+				}    
+			   
+				System.out.printf("\n After "+ITERATION_STEP+"th Step \n");
+			
+				for(k=1;k<=totalNodes;k++) 
+					System.out.printf(" Page Rank of "+k+" is :\t"+this.pagerank[k]+"\n"); 
+			
+				ITERATION_STEP = ITERATION_STEP+1;
+			}
+
+				// Add the Damping Factor to PageRank
+				for(k=1;k<=totalNodes;k++)
+				{
+					this.pagerank[k]=(1-DampingFactor)+ DampingFactor*this.pagerank[k]; 
+				} 
+			
+				// Display PageRank
+				System.out.printf("\n Final Page Rank : \n"); 
+				for(k=1;k<=totalNodes;k++)
+				{
+					System.out.printf(" Page Rank of "+k+" is :\t"+this.pagerank[k]+"\n"); 
+				}
+		
+		}
+
+	}
 
 
-
-
-
-
-
-
-  public class User
+	public class User
 	{
 		public int total_prefs;
 		public int id;
@@ -185,6 +181,12 @@ public class GoogleHappy
 		}
 
 	}
+
+	//primary list of people
+	public int count; //amount of people
+	public int teamsize;
+	public PageRank p;
+	public User[] c;
 	
 	//testing
 	public void getUserInfo(String userName)
@@ -196,38 +198,31 @@ public class GoogleHappy
 		}
 		
 	}
-
-
-  public class Team
-  {
-    public int max;
-    public int current;
-    public User people[];
-
-    Team(int n, User[] people)
-    {
-      max = n;
-      int count = 0;
-      for (User temp : people)
-      {
-        people[count] = temp;
-        count ++;
-      }
-      current = count;
-    }
-  }
-
-  
-
-
-
-
-  //primary list of people
-  public int count;
-  public int teamsize;
-  public PageRank p;
-  public User[] c;
-
+	
+	private User[] orderByPR(User[] usersAr)
+	{
+		for(int n = 0; n < count; n++)
+			for(int j = n; j < count; j++)
+				if(p.pagerank[n+1] > p.pagerank[j+1])
+				{
+					//perform swap
+					User temp = usersAr[n];
+					usersAr[n] = usersAr[j];
+					usersAr[j] = temp;
+					
+					j = n;
+				}
+				
+		//order them by how many prefs each has by putting them 
+		//while()
+		//{
+		//	if x 7
+		//}*/
+				
+		return usersAr;
+	}
+	
+	
 	public User[] teams;
 	private User[] fillTeams(User[] users)
 	{
@@ -261,6 +256,7 @@ public class GoogleHappy
 				choice=1;
 			}
 		}
+		printTeams(teams);
 		
 		User[] temp = new User[count];
 		//find those who didnt get places
@@ -344,11 +340,17 @@ public class GoogleHappy
 			}
 		}
 		
+		//put carter on a list because hes gerrys preference
+		for(int i = 0, p = 0; i < cut.length; i++)
+			System.out.println(cut[i].name);
+		
 		//puts the reaming people that didnt get a spot on to an empty team
 		for(int i = 0, p = 0; i < teams.length; i++)
 			if(teams[i].name.equals("NothingHere"))
 				teams[i] = cut[p];
 				
+		//go through array and put the remaining people that didnt get put in to a team, in a team that adds happiness
+		//if no team adds happiness then whatever team, doesnt matter
 		printTeams(teams);
 
 		return teams;
@@ -366,143 +368,141 @@ public class GoogleHappy
 		User fnUser = new User("getThisUserBroken", -1);
 		return fnUser;
 	}
-	
-  //reads from file and puts users into the people array
-  private void prefs(HashMap<String, Integer> mentioned_people)
-  {
-    count = 0;
-    c = new User[10000];
-    Scanner scanner = new Scanner(System.in);
-
-    String[] temp = new String[10000];
-
-    //reads lines into temp array
-    while(scanner.hasNextLine())
-    {
-      temp[count] = scanner.nextLine();
-      //processes line and add name to set
-      String[] temp2 = temp[count].split(",");
-
-      String[] temp3 = Arrays.copyOfRange(temp2, 1, temp2.length);
-
-      if(!mentioned_people.containsKey(temp2[0]))
-      {
-        c[count] = new User(temp2, count + 1);
-        mentioned_people.put(temp2[0],count); 
-
-      }
-      count ++;
-
-    }
-
-    int temp_n = count;
-    //creates users now that the total number of them is known
-    for (int i = 0; i < count; i++)
-    {
-
-      String[] temp2 = temp[i].split(",");
-
-      String[] temp3 = Arrays.copyOfRange(temp2, 1, temp2.length);
-      int len = temp3.length;
-
-      for (int j = 0; j < len; j++)
-      {
-        //goes through choices and if it is not in the map adds it
-        if (!mentioned_people.containsKey(temp3[j]))
-        {
-          temp[count] = temp3[j];
-          c[count] = new User(temp3[j], count + 1);
-          mentioned_people.put(temp3[j],count); 
-          count ++;
-
-        }
 
 
-      }
+	//reads from file and puts users into the people array
+	private void prefs(HashMap<String, Integer> mentioned_people)
+	{
+		count = 0;
+		c = new User[1000];
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		String[] temp = new String[1000];
 
-    for (int i = 0; i < count; i++)
-    {
-      String[] temp2 = temp[i].split(",");
-      String[] temp3 = Arrays.copyOfRange(temp2, 1, temp2.length);
+		//reads lines into temp array
+		while(scanner.hasNextLine())
+		{
+			temp[count] = scanner.nextLine();
+			//processes line and add name to set
+			String[] temp2 = temp[count].split(",");
 
-      //sets all paths to 0
-      for (int j = 0; j < count; j++)
-      {
-        p.path[i+1][j+1] = 0;
-      }
+			String[] temp3 = Arrays.copyOfRange(temp2, 1, temp2.length);
 
-      //chages paths based on existing prefs
-      for (int j = 0; j < temp3.length; j++)
-      {
-        int t = mentioned_people.get(temp3[j]);
-        p.path[i+1][t+1] = 1;
-        if (weighted == "t")
-        {
-          p.path[i+1][t+1] = j+1;
-        }
+			if(!mentioned_people.containsKey(temp2[0]))
+			{
+				c[count] = new User(temp2, count + 1);
+				mentioned_people.put(temp2[0],count); 
 
-      }
-    }
+			}
+			count ++;
+		}
 
-    for (int i = 1; i <= count; i++)
-    {
-      for (int j = 1; j <= count; j++)
-      {
-                //sets to zero if point at self
-        if(j==i)
-        {
-          p.path[i][j]=0;
-        }
-      }
+		int temp_n = count;
+		//creates users now that the total number of them is known
+		for (int i = 0; i < count; i++)
+		{
+			String[] temp2 = temp[i].split(",");
+			String[] temp3 = Arrays.copyOfRange(temp2, 1, temp2.length);
+			int len = temp3.length;
 
-    }
-    System.out.println("\n\n");
+			for (int j = 0; j < len; j++)
+			{
+				//goes through choices and if it is not in the map adds it
+				if (!mentioned_people.containsKey(temp3[j]))
+				{
+					temp[count] = temp3[j];
+					c[count] = new User(temp3[j], count + 1);
+					mentioned_people.put(temp3[j],count); 
+					count ++;
+				}
+		  }
 
-    for (int i = 1; i <= count; i++)
-    {
-      String line = "";
-      for (int j = 1 ; j <= count; j++)
-      {
-        line = line + p.path[i][j];
-      }
-      System.out.println(line);
-    }
+		}
 
-    scanner.close();
-  }
-  
-  public void primaryFunction(String w)
-  {
-    weighted = w;
-    teamsize = 0;
-    //defines map
-    HashMap<String, Integer> mentioned_people = new HashMap<String, Integer>();
-    p = new PageRank();
+		for (int i = 0; i < count; i++)
+		{
+			String[] temp2 = temp[i].split(",");
+			String[] temp3 = Arrays.copyOfRange(temp2, 1, temp2.length);
 
-    //loads the users and their prefs
-    prefs(mentioned_people);
+			//sets all paths to 0
+			for (int j = 0; j < count; j++)
+			{
+				p.path[i+1][j+1] = 0;
+			}
 
-    fillTeams(c);
-    
-    //runs page rank
-    p.calc(count);
+			//chages paths based on existing prefs
+			for (int j = 0; j < temp3.length; j++)
+			{
+				int t = mentioned_people.get(temp3[j]);
+				p.path[i+1][t+1] = 1;
+				if (weighted == "t")
+				{
+					p.path[i+1][t+1] = j+1;
+				}
+			}
+		}
 
-    //potential loop for multiple itterations???
+		for (int i = 1; i <= count; i++)
+		{
+			for (int j = 1; j <= count; j++)
+			{
+				//sets to zero if point at self
+				if(j==i)
+				{
+					p.path[i][j]=0;
+				}
+			}
+		}
+		System.out.println("\n\n");
 
-    //run team optimization algorithm
+		for (int i = 1; i <= count; i++)
+		{
+			String line = "";
+			for (int j = 1 ; j <= count; j++)
+			{
+				line = line + p.path[i][j];
+			}
+			System.out.println(line);
+		}
 
-    //output (with verbosity??)
-  }
+		scanner.close();
+	  }
+	  
+	  public void primaryFunction(String w)
+	  {
+		weighted = w;
+		teamsize = 3;
+		//defines map
+		HashMap<String, Integer> mentioned_people = new HashMap<String, Integer>();
+		p = new PageRank();
 
-  public static void main (String[] args )  throws FileNotFoundException
-  {
-    //creates the main GoogleHappy object and runs primaryFunction
-    new GoogleHappy().primaryFunction(args[0]);
-  }
-  
-  	public void printTeams(User[] users)
+		//loads the users and their prefs into the c array
+		prefs(mentioned_people);
+		
+		//runs page rank
+		p.calc(count);
+		
+		
+		//orderByPR(c);
+		fillTeams(c);
+		
+		//printTeams(c);
+
+		//potential loop for multiple itterations???
+
+		//run team optimization algorithm
+
+		//output (with verbosity??)
+	  }
+
+	  public static void main (String[] args )  throws FileNotFoundException
+	  {
+			//creates the main GoogleHappy object and runs primaryFunction
+			new GoogleHappy().primaryFunction(args[0]);
+	  }
+	  
+	  
+	  public void printTeams(User[] users)
 	  {
 		User fnUser = new User("NothingHere", -1);
 		//print out the names and divide them into groups of 3
